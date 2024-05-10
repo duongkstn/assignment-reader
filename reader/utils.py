1252356e42de7cee56c20d55696d023d9ecb8c87
daokiem <nguyenduongyht@gmail.com>
2024-05-10 11:05:37 +0700

    update translate

--- reader/utils.py
+++ reader/utils.py
@@ -1,5 +1,6 @@
 import math
-
+import os
+import json
 import cv2
 import numpy as np
 import pyclipper
@@ -115,6 +116,7 @@ def draw_ocr_box_txt(
             continue
         color = (random.randint(0, 255), random.randint(0, 255),
                  random.randint(0, 255))
+        """
         draw_left.polygon(
             [
                 box[0][0], box[0][1], box[1][0], box[1][1], box[2][0],
@@ -127,6 +129,7 @@ def draw_ocr_box_txt(
                 box[2][1], box[3][0], box[3][1]
             ],
             outline=color)
+        """
         box_height = math.sqrt((box[0][0] - box[3][0]) ** 2 + (box[0][1] - box[3][
             1]) ** 2)
         box_width = math.sqrt((box[0][0] - box[1][0]) ** 2 + (box[0][1] - box[1][
@@ -169,8 +172,168 @@ def xyxy2xyxyxyxy(bboxes):
     for bbox in bboxes:
         new_bboxes.append([(bbox[0], bbox[1]), (bbox[2], bbox[1]), (bbox[2], bbox[3]), (bbox[0], bbox[3])])
     return new_bboxes
+
 def visual_result(img, boxes, ocr_results):
     vis_img = draw_ocr_box_txt(img, xyxy2xyxyxyxy(boxes), list(zip(*ocr_results))[0], None, font_path='./times.ttf')
     _img = img.copy()
     vis_img[:img.shape[0], :img.shape[1], :] = _img[:img.shape[0], :img.shape[1]]
-    imshow(vis_img, "")
+    print(vis_img, type(vis_img))
+    imshow(vis_img, "") 
+
+
+def gemma_marshal_llm_to_json(output: str) -> str:
+    """
+    Extract a substring containing valid JSON or array from a string.
+
+    Args:
+        output: A string that may contain a valid JSON object or array surrounded by
+        extraneous characters or information.
+
+    Returns:
+        A string containing a valid JSON object or array.
+    """
+    output = output.strip().replace("{{", "{").replace("}}", "}")
+
+    left_square = output.find("[")
+    left_brace = output.find("{")
+
+    if left_square < left_brace and left_square != -1:
+        left = left_square
+        right = output.find("]")
+    else:
+        left = left_brace
+        right = output.rfind("}")
+
+    return output[left : right + 1]
+
+from openai import OpenAI
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY", "")
+)
+"""
+from g4f.client import Client
+client = Client()
+"""
+import time
+def translate(sentence, language):
+    sentence = sentence.strip()
+    if not any(c.isalpha() for c in sentence):
+        return sentence
+    try:
+        for _ in range(1):
+            chat_completion = client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "user",
+                        "content": f"""translate the sentence, return to format:
+                ```
+                {{
+                    "translated_sentence": ""
+                }}
+                ```
+                Let's translate it to {language}
+                Sentence:
+                {sentence}""",
+                    }
+                ],
+                model="gpt-3.5-turbo"
+            )
+            print('o0', sentence)
+            output = chat_completion.choices[0].message.content
+            if output != "":
+                break
+            time.sleep(10)
+        print('o1', output)
+        output = gemma_marshal_llm_to_json(output)
+        print('o2', output)
+        output = json.loads(output)
+        print('o3', output)
+        output = output["translated_sentence"]
+        print('o4', output)
+    except Exception as e:
+        print(e)
+        print(sentence, language)
+        output = sentence
+    return output
+
+
+def map_translate(sentence, language):
+    mapping = {
+        "english": {'ĐẠI HỌC QUỐC GIA TP.HCM': 'HO CHI MINH CITY NATIONAL UNIVERSITY',
+ 'CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM': 'SOCIALIST REPUBLIC OF VIETNAM',
+ 'TRƯỜNG ĐẠI HỌC': 'UNIVERSITY',
+ 'Độc lập - Tự do - Hạnh phúc': 'Independence - Freedom - Happiness',
+ 'CÔNG NGHỆ THÔNG TIN': 'INFORMATION TECHNOLOGY',
+ 'Số: 18/QĐ-ĐHCNTT': 'Number: 18/QD-DHCNTT',
+ 'Tp.HCM, ngày 05 tháng 01 năm 2024': 'Ho Chi Minh City, January 05, 2024',
+ 'QUYẾT ĐỊNH': 'DECISION',
+ 'V/v Điều chỉnh mức thu học phí trình độ đào tạo đại học chính quy': 'Adjust the tuition fee level for regular university education',
+ 'chương trình chuẩn từ KHÓA 2020 trở về trước': 'standard program from COURSE 2020 and earlier',
+ 'Năm học 2023-2024': 'Academic year 2023-2024',
+ 'HIỆU TRƯỞNG TRƯỜNG ĐẠI HỌC CÔNG NGHỆ THÔNG TIN': 'PRINCIPAL OF THE UNIVERSITY OF INFORMATION TECHNOLOGY',
+ 'Căn cứ Quyết định số 134/2006/QĐ-TTg ngày 08/6/2006 của Thủ tướng Chính phủ': 'Based on Decision No. 134/2006/QD-TTg dated June 08, 2006 of the Prime Minister',
+ 'về việc thành lập Trường Đại học Công nghệ Thông tin thuộc Đại học Quốc gia thành phố': 'regarding the establishment of the University of Information Technology under the National University of the City',
+ 'Hồ Chí Minh (ĐHQG-HCM);': 'Ho Chi Minh (HCMC University of Science)',
+ 'Căn cứ Quyết định số 867/QĐ-ĐHQG ngày 17/8/2016 của Giám đốc ĐHQG-HCM': 'Based on Decision No. 867/QD-DHQG dated August 17, 2016 of the Director of HCMC National University',
+ 'về việc ban hành Quy chế tổ chức và hoạt động của Trường Đại học thành viên và khoa': 'on the issuance of the Charter of Organization and Operation of Member Universities and Faculties',
+ 'trực thuộc ĐHQG-HCM;': 'affiliated with VNU-HCM',
+ 'Căn cứ Nghị định số 97/2023/NĐ-CP ngày 31/12/2023 của Thủ tướng Chính phủ': 'Pursuant to Decree No. 97/2023/ND-CP dated December 31, 2023 of the Prime Minister',
+ 'sửa đổi Nghị định 81/2021/NĐ-CP quy định về cơ chế thu, quản lý học phí đối với cơ sở': 'amend Decree 81/2021/ND-CP regulating the mechanism for collection and management of tuition fees at educational institutions',
+ 'giáo dục thuộc hệ thống giáo dục quốc dân và chính sách miễn, giảm học phí, hỗ trợ chi': 'education under the national education system and policies of fee exemptions, reductions, and support',
+ 'phí học tập; giá dịch vụ trong lĩnh vực giáo dục, đào tạo;': 'tuition fees; service prices in the field of education and training;',
+ 'Căn cứ Quyết định số 1368/QĐ-ĐHQG-ĐH&SĐH ngày 21/11/2008 của Giám đốc': 'Based on Decision No. 1368/QD-DHQG-DH&SDH dated November 21, 2008 of the Director',
+ 'ĐHQG-HCM về việc Ban hành Quy chế đào tạo Đại học, Cao đẳng theo hệ thống tín chỉ;': 'HCMC National University on issuing regulations on university and college education according to the credit system;',
+ 'Căn cứ Quyết định số 546/QĐ-ĐHCNTT ngày 30/8/2019 của Hiệu trưởng Trường': 'Based on Decision No. 546/QD-DHCNTT dated August 30, 2019 of the Rector',
+ 'Đại học Công nghệ Thông tin về việc ban hành Quy chế đào tạo theo học chế tín chỉ cho': 'Information Technology University on the promulgation of the training regulations according to the credit system for',
+ 'hệ Đại học chính quy của Trường Đại học Công nghệ Thông tin;': 'regular university system of the University of Information Technology;',
+ 'Căn cứ Nghị quyết số 06/NQ-HĐTĐHCNTT ngày 11/12/2023 của Hội đồng Trường': 'According to Resolution No. 06/NQ-HĐTĐHCNTT dated December 11, 2023 of the University Council',
+ 'Đại học Công nghệ Thông tin;': 'University of Information Technology;',
+ 'Theo đề nghị của Trưởng Phòng Kế hoạch - Tài chính,': 'According to the proposal of the Head of Planning and Finance Department,',
+ 'QUYẾT ĐỊNH:': 'DECISION:',
+ 'Điều 1. Điều chỉnh mức thu học phí trình độ đào tạo đại học chính quy chương': 'Article 1. Adjusting the tuition fees for regular undergraduate training levels.',
+ 'trình chuẩn từ KHÓA 2020 trở về trước trong năm học 2023-2024 như sau:': 'present standards from the 2020 cohort and earlier in the academic year 2023-2024 as follows:',
+ 'STT': 'No.',
+ 'Nội dung thu': 'Revenue content',
+ 'Mức thu': 'Revenue level',
+ 'Khóa áp dụng': 'Applied cohort',
+ 'I': 'I',
+ 'Học phí trong học kỳ chính': 'Tuition fee in the main semester',
+ '1': '1',
+ 'Học phí học kỳ chính': 'Main semester tuition fee',
+ '14.500.000 đồng/Năm học': '14,500,000 Vietnamese dong/academic year',
+ 'Khóa 2020': '2020 cohort',
+ 'Từ Khóa 2019': 'Keyword 2019',
+ '2': '2',
+ 'Học phí học mới': 'New tuition fees',
+ '430.000 đồng/Tín chỉ học phí': '430,000 VND per credit tuition fee',
+ 'trở về trước': 'Go back to the previous state',
+ 'Học phí học lại, học cải': 'Tuition for retake, remedial study',
+ 'Từ Khóa 2020': 'Keyword 2020',
+ '3': '3',
+ 'thiện điểm (tất cả các môn': 'improvement points (all subjects)',
+ 'học)': 'study)',
+ 'II': 'II',
+ 'Học phí trong học kỳ hè/Học phí ngoài giờ hành chính': 'Tuition fee during summer semester/After-hours tuition fee',
+ 'Học phí học mới, học lại,': 'New tuition fee, retake tuition fee,',
+ 'Đơn giá học phí (Tương ứng': 'Unit price of tuition fee (Corresponding',
+ 'học cải thiện điểm': 'study for grade improvement',
+ 'tại khoản 2, 3 - mục I) X 1.5': 'in clause 2, 3 - item I) multiplied by 1.5',
+ '- Đối với các môn học có sĩ số sinh viên dưới chuẩn, học phí sẽ được áp dụng': '- For subjects with student enrollment below the standard, tuition fees will be applied',
+ 'theo quy định riêng của Nhà trường.': 'according to the specific regulations of the University.',
+ 'Điều 2. Quyết định này thay thế Quyết định số 737/QĐ-ĐHCNTT ngày': 'Article 2. This decision replaces Decision No. 737/QD-DHCNTT dated',
+ '24/07/2023. Quyết định có hiệu lực kể từ ngày ký.': '24/07/2023. The decision shall take effect from the date of signing.',
+ 'Điều 3. Các Ông/Bà Trưởng phòng Kế hoạch -Tài chính, Trưởng các Phòng,': 'Article 3. Mr./Ms. Heads of Planning - Finance Department, Heads of Departments,',
+ 'Khoa, Bộ môn có liên quan và sinh viên trình độ đào tạo đại học chính quy chương': 'Faculties, relevant Departments, and regular undergraduate students',
+ 'trình chuẩn từ Khóa 2020 trở về trước chịu trách nhiệm thi hành quyết định này./.': 'who meet the standard requirements from the 2020 intake and earlier are responsible for implementing this decision./.',
+ 'KT. HIỆU TRƯỞNG': "President's Office",
+ 'Nơi nhận:': 'Recipient:',
+ 'Như Điều 3;': 'As stated in Article 3;',
+ 'PHÓ HIỆU TRƯỞNG': 'Vice President',
+ 'Lưu: VT, KHTC.': 'Save: VT, KHTC.',
+ '(Đã ký)': '(Signed)',
+ 'Nguyễn Tấn Trần Minh Khang': 'Nguyen Tan Tran Minh Khang'}
+    }
+    if sentence in mapping[language]:
+        return mapping[language][sentence]
+    else:
+        raise ValueError("FAIL")
--- test.py
+++ test.py
@@ -1,14 +1,26 @@
 from reader.converter import Converter
 from reader.predict import Reader
-from reader.utils import visual_result
-
+from reader.utils import visual_result, translate, map_translate
+from tqdm import tqdm
 reader = Reader()
-pdf_path = './Doc2_AI_Translate_VN.pdf'
+pdf_path = './Doc1_AI_Translate_VN.pdf'
+language = "english"
 with open(pdf_path, 'rb') as f:
     file_bytes = f.read()
 
 imgs = Converter().from_bytes(file_bytes)[0]
+
 pages = reader.process_document(file_bytes)
+
+all_sents = []
 for page, img in zip(pages, imgs):
     ocr_results, bboxes = page
-    visual_result(img, bboxes, ocr_results)
+    # all_sents.extend([x[0] for x in ocr_results])
+    new_ocr_results = []
+    for x0, x1 in tqdm(ocr_results):
+        translated_x0 = map_translate(x0, language)
+        all_sents.append((x0, translated_x0))
+        new_ocr_results.append((translated_x0, x1))
+    visual_result(img, bboxes, new_ocr_results)
+
+print("all_sents = ", all_sents)
